fe8e45983ecb4e8d09bcad784895c652f352bcfb
sharask <sarunask@gmail.com>
2024-01-12 12:29:28 +0200

    New file

--- draw.py
+++ draw.py
@@ -11,4 +11,19 @@ cv.imshow('Blank', blank)
 blank[:] = 0,255,0
 cv.imshow('Green', blank)
 
+ # Paint a square inside the image
+blank[:] = 0, 0, 0
+blank[200:300, 300:400] = 0, 0, 255
+cv.imshow('Red Square', blank)
+
+#2. Paint a Rectangle
+blank[:] = 0, 0, 0
+cv.rectangle(blank, (10,10), (250,350), (0,255,0), thickness=2)
+cv.imshow('Rectangle', blank)
+
+
+# Write text
+cv.putText(blank, 'Hello', (225,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
+cv.imshow('Text', blank)
+
 cv.waitKey(0)
